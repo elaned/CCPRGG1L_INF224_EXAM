136d8df85879c3e56a51c57a8a1577f7b01f1b86
Elane Dela Cruz <111546358+elaned@users.noreply.github.com>
2022-09-06 09:36:06 +0800

    Create App.java

---
+++ exam_1/App.java
@@ -0,0 +1,26 @@
+public class App {
+    public static void main(String[] args) {
+        
+        // Character Class
+
+        Student me = new Student();
+
+        me.surname = "Dela Cruz";
+        me.firstname = "Elane";
+        me.middleInitial = 'M';
+        me.dateOfBirth ="11/18/2001";
+        me.studentNumber = 2022100972;
+        me.studentEmailAddress = "delacruzem@students.national-u.com";
+        me.iamAwesome = true;
+        me.sayMystudentNumber();
+        me.sayMystudentEmailAddress();
+        me.sayMystudentiamAwesome();
+
+    
+
+
+
+    
+
+    }
+}
