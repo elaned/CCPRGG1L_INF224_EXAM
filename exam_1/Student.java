d49d98e7bd7ecf8dba1e2205f6f2a406ad63f1d9
Elane Dela Cruz <111546358+elaned@users.noreply.github.com>
2022-09-06 09:36:43 +0800

    Create Student.java

---
+++ exam_1/Student.java
@@ -0,0 +1,42 @@
+public class Student {
+
+
+    // Student Attributes
+    String surname;
+    String firstname;
+    char midddleInitial;
+    String dateOfBirth;
+    int studentNumber;
+    String studentEmailAddress;
+    boolean iamAwesome;
+    public char middleInitial;
+
+    // Student Method
+    public void sayMysurname() {
+        System.out.println("My Surname is " + surname);
+    } 
+
+    public void sayMyfirstname() {
+        System.out.println("My First Name is " + firstname);
+    } 
+
+    public void sayMymiddleInitial() {
+        System.out.println("My Middle Initial is " + middleInitial);
+    } 
+
+    public void sayMyDateOfBirth() {
+        System.out.println("My Birthday is " + dateOfBirth);
+    } 
+
+    public void sayMystudentNumber() {
+        System.out.println("My Student Number is " + studentNumber);
+    } 
+
+    public void sayMystudentEmailAddress() {
+        System.out.println("My Student Email is " + studentEmailAddress);
+    } 
+
+    public void sayMystudentiamAwesome() {
+        System.out.println("I am Awesome " + iamAwesome);
+    } 
+}
